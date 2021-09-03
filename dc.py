81c9634401c79ef599682823654a757c64c3900b
thackerdynasty <70179471+thackerdynasty@users.noreply.github.com>
2021-09-03 18:08:32 -0400

    Code for non-public beta

--- dc.py
+++ dc.py
@@ -1,1 +1,14 @@
-/Users/Vedika/Documents/
+import random
+print("Hello! Let's play crack the code!")
+print("I am going to pick a five digit code. Your job is to crack it!")
+doorcode = random.randint(10000,99999)
+correctcode = False
+while not correctcode:
+    global correctcode
+    global doorcode
+    usercode = int(input("Guess."))
+    if usercode == doorcode:
+        correctcode = True
+    else:
+        print("Wrong code! Try again.")
+print("You got it right!")
